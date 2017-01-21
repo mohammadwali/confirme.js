94e0290f803466890af7f8286da4f669f39de4f3
Mohammad Wali <mohdwali@live.com>
2017-01-22 04:15:38 +0530

    Update

--- jquery.confirMe.js
+++ jquery.confirMe.js
@@ -18,7 +18,7 @@
  *     classes: {
  *         custom: ["subscribe-button"]
  *     },
- *     on: function(type, closeModal, button, event) {
+ *     on: function(name, closeModal, button, event) {
  *
  *     }
  * })
@@ -39,7 +39,11 @@
 })(window, function (window, $, utils) {
 
     const ESCAPE_KEY_CODE = 27;
-    const DEFAULT_BUTTON_CLASSES = ["confirMe-button"];
+    const REMOVE_DELAY = 200;
+    const INIT_DELAY = 150;
+    const DEFAULT_BUTTON_CLASS = "confirMe-button";
+
+    var avialableModals = [];
 
     var defaultOptions = {};
     defaultOptions.message = "Are you sure you want to do this ?";
@@ -58,12 +62,95 @@
     function confirMe(options) {
         var settings = utils.mergeObj(defaultOptions, options);
 
+        ///generating buttons and doing rest of stuff
+        var data = generateModal(settings);
 
+        ///setting up message
+        data.selectors.messageContainer.text(settings.message);
+
+        //caching modal object
+        avialableModals.push(data);
+
+        //pushing modal to body
+        $("body").append(data.selectors.modal);
+
+        //initialize
+        setTimeout(function () {
+            //binding buttons
+            data.selectors.buttons.on("click", clickHandler);
+
+            data.selectors.modal.addClass("is-visible");
+        }, INIT_DELAY);
+
+
+        //////////////////
+        function clickHandler(event) {
+            var button = $(this);
+            var name = button.data("confirMeName");
+
+            utils.dispatch(this, [name, closeModal, button, event], settings.on);
+
+            function closeModal() {
+                data.selectors.modal.removeClass("is-visible");
+
+                setTimeout(function () {
+
+                    data.selectors.modal.remove();
+
+                    flush();
+
+                }, REMOVE_DELAY);
+
+            }
+        }
+
+        function flush() {
+            $.each(avialableModals, function (modalObject, index) {
+                if (modalObject.id === data.id) {
+                    avialableModals.splice(index, 1);
+                }
+            });
+
+            data.selectors.buttons.off("click", clickHandler);
+            data = null;
+        }
     }
 
+    function generateModal(settings) {
+        var wrapper = $("<div class='confirme-popup' role='alertdialog'></div>");
+        var container = $("<div class='confirme-popup-container'></div>");
+        var messageContainer = $("<p></p>");
+        var buttonsContainer = $("<ul class='confirme-buttons list-inline m-n p-b p-l p-r'>");
+        var wrapperId = utils.randId();
 
-    function triggerCallback() {
+        container.append(messageContainer);
+        container.append(buttonsContainer);
+        wrapper.append(container);
+        wrapper.attr("id", "confirMe-" + wrapperId);
 
+        $.each(settings.labels, function (name, text) {
+            var li = $("<li></li>");
+            var a = $("<a class='btn btn-md rounded w-xs'></a>");
+
+            a.text(text)
+                .addClass(DEFAULT_BUTTON_CLASS)
+                .addClass((name === "confirm" ? "confirme-popup-confirm" : name === "cancel" && "confirme-popup-cancel" || ""))
+                .data("confirMeName", name);
+
+            li.append(a);
+
+            buttonsContainer.append(li);
+        });
+
+        return {
+            id: wrapperId,
+            selectors: {
+                modal: wrapper,
+                buttons: wrapper.find(".".concat(DEFAULT_BUTTON_CLASS)),
+                buttonsContainer: buttonsContainer,
+                messageContainer: messageContainer
+            }
+        }
     }
 
 }, function () {
@@ -84,6 +171,9 @@
         },
         dispatch: function (scope, arguments, callback) {
             return callback.apply(scope, arguments);
+        },
+        randId: function () {
+            return Math.random().toString(36).substr(2, 10);
         }
     };
 }());
